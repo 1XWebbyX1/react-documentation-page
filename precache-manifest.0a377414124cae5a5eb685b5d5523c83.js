7fcadc2eb40dfb6cd28bfc12b4551a42c820efe2
Khayati Asrani <devinc@TheDen-3.local>
2019-01-07 18:13:27 +0530

    Updates

--- precache-manifest.0a377414124cae5a5eb685b5d5523c83.js
+++ precache-manifest.0a377414124cae5a5eb685b5d5523c83.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "2524aa876ac413730cee",
-    "url": "/react-documentation-page/static/css/main.7b631ca9.chunk.css"
+    "revision": "484e8edbe66bd99c5436",
+    "url": "/react-documentation-page/static/css/main.5a29ff49.chunk.css"
   },
   {
-    "revision": "2524aa876ac413730cee",
-    "url": "/react-documentation-page/static/js/main.2524aa87.chunk.js"
+    "revision": "484e8edbe66bd99c5436",
+    "url": "/react-documentation-page/static/js/main.484e8edb.chunk.js"
   },
   {
     "revision": "35569de84efa60fd2998",
@@ -72,7 +72,7 @@ self.__precacheManifest = [
     "url": "/react-documentation-page/static/js/runtime~main.53d7d208.js"
   },
   {
-    "revision": "f4a28560c6bbc1995e10d30c891a642c",
+    "revision": "7b676fbdcfdb36ba399f734e76394554",
     "url": "/react-documentation-page/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-documentation-page/precache-manifest.d373ad0d7e12b012e33c9e221f52422c.js"
+  "/react-documentation-page/precache-manifest.0a377414124cae5a5eb685b5d5523c83.js"
 );
 
 workbox.clientsClaim();
